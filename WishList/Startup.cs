17b388bea3f4730d92a32c96051a179296e906e0
Ed Roozendaal <passport@roozendaal.org>
2020-07-30 22:31:59 +0200

    AddExceptionHandler fix

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,8 +32,8 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseRouting()
-                .UseEndpoints(endpoints =>
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
                 {
                     endpoints.MapDefaultControllerRoute();
                 });
